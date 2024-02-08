27f8f3bea18da8a6bd7780243022e8f4fc5f3454
Devran Yılmaz <mdevranyilmaz1@gmail.com>
2024-02-08 10:03:02 +0300

    average and subtract average methods
    I wrote the find average method and find subtract average method.

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args){
 
+
+
+
+
+
+        public static int[] findSubstractAverage(int[] array) {
+            int average = findAverage(array);
+            int[] newArray = new int[array.length];
+            for(int i = 0; i < array.length; i++) {
+                newArray[i] = array[i] - average;
+            }
+            return newArray;
+        }
+        
+        public static int findAverage(int[] array) {
+            int sum;
+            for(int elements : array) {
+                sum += elements;
+            }
+            return sum / array.length;
+        }
     }
 }
