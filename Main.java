b607b17d386e4a9c2ea6c24b0e82d4aeb6012284
Rıfat Arifoğlu <rifatarifoglu38@gmail.com>
2024-02-08 10:10:20 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -45,4 +45,31 @@ public class Main {
         }
         return result;
     }
+
+    public static int findMin(int[] array)
+    {
+        int size = array.length;
+        int min = 0;
+        for(int i = 0; i < size; i++)
+        {
+            if(array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+    public static int findMax(int[] array)
+    {
+        int size = array.length;
+        int max = 0;
+        for(int i = 0; i < size; i++)
+        {
+            if(array[i] > max)
+            {
+                max = array[i];
+            }
+        }
+        return max;
+    }
 }
