36df9abaa81a77aa7184c6d1448a5f8ecb36e74c
Selim Arslan <ardaselimarslan@gmail.com>
2024-02-08 10:02:38 +0300

    odd even sum methods

--- Main.java
+++ Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args){
 
+        int[] arr = new int[5];
+        arr[0] = 5;
+        arr[1] = 3;
+        arr[2] = 1;
+        arr[3] = 0;
+        arr[4] = 6;
+
+        System.out.println(sumOfOdd(arr));
+        System.out.println(sumOfEven(arr));
+
+    }
+    public static int sumOfOdd(int[] arr) {
+        int result = 0;
+        int length = arr.length;
+        for(int i = 1; i < length; i = i + 2) {
+            result += arr[i];
+        }
+        return result;
+    }
+
+    public static int sumOfEven(int[] arr) {
+        int result = 0;
+        int length = arr.length;
+        for(int i = 0; i < length; i = i + 2) {
+            result += arr[i];
+        }
+        return result;
     }
 }
